88e274edb4e8551919802ce9909766dea7ca033f
Jeandeng47 <dengziying98@gmail.com>
2024-03-12 14:20:03 -0400

    revised Ex_2_1_05 Ex_2_1_03

--- textbook exercises/Chapter_2.1/Ex_2_1_05.java
+++ textbook exercises/Chapter_2.1/Ex_2_1_05.java
@@ -41,7 +41,7 @@ public class Ex_2_1_05 {
     }
 
     public static void main(String[] args) {
-        Integer[] decrArray = { 10, 9, 8, 7, 6, 5, 4, 3, 1 };
+        Integer[] decrArray = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
         Integer[] incrArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         Integer[] randomArray = new Integer[10];
         Random rand = new Random();
